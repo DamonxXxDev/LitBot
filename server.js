69dda25b80e601bbe80274ddcf0f6989c4b4f265
Toni T <tonitert@gmail.com>
2017-12-17 12:24:03 +0200

    help command fixes

--- server.js
+++ server.js
@@ -281,7 +281,7 @@ function getAPL(msg){
       });
 }
 const commands = {
-  //TODO autoplaylistremove, check permissions every command, same guild files in 1 file, autoplaylist in help file, change prefix by guild
+  //TODO autoplaylistremove, check permissions every command, same guild files in 1 file, change prefix by guild
     'autoplaylist': (msg) => {
       var times = 0;
       next();
@@ -851,7 +851,7 @@ const commands = {
         }
     },
     'help': (msg) => {
-        let tosend = ['```xl', tokens.prefix + 'join: "Join voice channel of message sender."', tokens.prefix + 'queue: "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play: "Play a song. Enter search terms or link after this command. "', tokens.prefix + 'autoplaylist: "Show songs in autoplaylist. "', 'Bot Controller commands:', tokens.prefix + 'autoplaylistadd, apladd: "Add a song to the autoplaylist. Enter search terms or youtube url after this command. "',tokens.prefix + 'autoplaylistremove, aplremove: "Remove a song from autoplaylist. Enter URL after this command. You can see the URLs of the songs in the autoplaylist with ' + tokens.prefix + 'autoplaylist."', tokens.prefix + 'addcommand: "Adds a custom command, example: ' + tokens.prefix + 'addcommand (command here) (response here)"', tokens.prefix + 'removecommand: "Removes a custom command, example: ' + tokens.prefix + '(command)"', tokens.prefix + 'shuffle: "Shuffles queue."', tokens.prefix + 'loopqueue: "Puts queue on loop."', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause: "Pauses the music."', tokens.prefix + 'resume: "Resumes the music."', tokens.prefix + 'skip: "Skips the playing song."', tokens.prefix + 'time: "Shows the playtime of the song."', 'volume+(+++): "Increases volume by 2%."', 'volume-(---): "Decreases volume by 2%."', '```'];
+        let tosend = ['```xl', tokens.prefix + 'weather <city>: "Get weather in city."', tokens.prefix + 'steam sale: "Shows the next Steam sale."' , tokens.prefix + 'join: "Join voice channel of message sender."', tokens.prefix + 'queue: "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play: "Play a song. Enter search terms or link after this command. "', tokens.prefix + 'autoplaylist: "Show songs in autoplaylist. "', 'Bot Controller commands:', tokens.prefix + 'autoplaylistadd, apladd <song>: "Add a song to the autoplaylist. Enter search terms or youtube url after this command. "',tokens.prefix + 'autoplaylistremove, aplremove <url>: "Remove a song from autoplaylist. Enter URL after this command. You can see the URLs of the songs in the autoplaylist with ' + tokens.prefix + 'autoplaylist."', tokens.prefix + 'addcommand <command> <response>: "Adds a custom command."', tokens.prefix + 'removecommand <command>: "Removes a custom command."', tokens.prefix + 'shuffle: "Shuffles queue."', tokens.prefix + 'loopqueue: "Puts queue on loop."', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause: "Pauses the music."', tokens.prefix + 'resume: "Resumes the music."', tokens.prefix + 'skip: "Skips the playing song."', tokens.prefix + 'time: "Shows the playtime of the song."', 'volume+(+++): "Increases volume by 2%."', 'volume-(---): "Decreases volume by 2%."', '```'];
         msg.channel.send(tosend.join('\n'));
     },
     /*'reboot': (msg) => {
@@ -1174,7 +1174,7 @@ function checkRoleIds(){
   var guildArray = guilds.array();
   //check if bot has been added to guild while offline
   for (var i = 0; i < guildArray.length; i++){
-    function check(id) {s
+    function check(id) {
     return id.guildid == guildArray[i].id;
     }
     if (roleids.find(check) == undefined){
