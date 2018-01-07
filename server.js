d8d0104f343f98342049e2b55667fa085514ec97
Toni T <tonitert@gmail.com>
2018-01-07 13:43:37 +0200

    added info command

--- server.js
+++ server.js
@@ -882,8 +882,54 @@ const commands = {
         }
     },
     'help': (msg) => {
-        let tosend = ['```xl', tokens.prefix + 'chooserole: "Choose a Bot Controller role. If a Bot Controller role is set, requires the Bot Controller role."', tokens.prefix + 'fortnite stats <psn/xbl/pc> <username> <all/solo/duo/squad>: "Get fortnite stats of player in chosen gamemode."', tokens.prefix + 'weather <city>: "Get weather in city."', tokens.prefix + 'steam sale: "Shows the next Steam sale."' , tokens.prefix + 'join: "Join voice channel of message sender."', tokens.prefix + 'queue: "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play: "Play a song. Enter search terms or link after this command. "', tokens.prefix + 'autoplaylist: "Show songs in autoplaylist. "', 'Bot Controller commands:', tokens.prefix + 'autoplaylistadd, apladd <song>: "Add a song to the autoplaylist. Enter search terms or youtube url after this command. "',tokens.prefix + 'autoplaylistremove, aplremove <url>: "Remove a song from autoplaylist. Enter URL after this command. You can see the URLs of the songs in the autoplaylist with ' + tokens.prefix + 'autoplaylist."', tokens.prefix + 'addcommand <command> <response>: "Adds a custom command."', tokens.prefix + 'removecommand <command>: "Removes a custom command."', tokens.prefix + 'shuffle: "Shuffles queue."', tokens.prefix + 'loopqueue: "Puts queue on loop."', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause: "Pauses the music."', tokens.prefix + 'resume: "Resumes the music."', tokens.prefix + 'skip: "Skips the playing song."', tokens.prefix + 'time: "Shows the playtime of the song."', 'volume+(+++): "Increases volume by 2%."', 'volume-(---): "Decreases volume by 2%."', '```'];
+        let tosend = ['```xl', tokens.prefix + 'info: "Shows uptime, bot creator, invite link and the github repository."', tokens.prefix + 'chooserole: "Choose a Bot Controller role. If a Bot Controller role is set, requires the Bot Controller role."', tokens.prefix + 'fortnite stats <psn/xbl/pc> <username> <all/solo/duo/squad>: "Get fortnite stats of player in chosen gamemode."', tokens.prefix + 'weather <city>: "Get weather in city."', tokens.prefix + 'steam sale: "Shows the next Steam sale."' , tokens.prefix + 'join: "Join voice channel of message sender."', tokens.prefix + 'queue: "Shows the current queue, up to 15 songs shown."', tokens.prefix + 'play: "Play a song. Enter search terms or link after this command. "', tokens.prefix + 'autoplaylist: "Show songs in autoplaylist. "', 'Bot Controller commands:', tokens.prefix + 'autoplaylistadd, apladd <song>: "Add a song to the autoplaylist. Enter search terms or youtube url after this command. "',tokens.prefix + 'autoplaylistremove, aplremove <url>: "Remove a song from autoplaylist. Enter URL after this command. You can see the URLs of the songs in the autoplaylist with ' + tokens.prefix + 'autoplaylist."', tokens.prefix + 'addcommand <command> <response>: "Adds a custom command."', tokens.prefix + 'removecommand <command>: "Removes a custom command."', tokens.prefix + 'shuffle: "Shuffles queue."', tokens.prefix + 'loopqueue: "Puts queue on loop."', 'the following commands only function while the play command is running:'.toUpperCase(), tokens.prefix + 'pause: "Pauses the music."', tokens.prefix + 'resume: "Resumes the music."', tokens.prefix + 'skip: "Skips the playing song."', tokens.prefix + 'time: "Shows the playtime of the song."', 'volume+(+++): "Increases volume by 2%."', 'volume-(---): "Decreases volume by 2%."', '```'];
         msg.channel.send(tosend.join('\n'));
+    },
+    'info': (msg) => {
+      var seconds = Math.floor(process.uptime());
+      var days = Math.floor(seconds / (3600*24));
+      seconds  -= days*3600*24;
+      var hrs   = Math.floor(seconds / 3600);
+      seconds  -= hrs*3600;
+      var mnts = Math.floor(seconds / 60);
+      seconds  -= mnts*60;
+      msg.channel.send({
+        "embed": {
+          "description": "**Bot Info**",
+          "color": 3145472,
+          /*"footer": {
+            "text": "Times in UTC/GMT. Source: https://www.whenisthenextsteamsale.com/"
+          },*/
+          "author": {
+            "name": msg.author.username + "#" + msg.author.discriminator,
+            "icon_url": msg.author.avatarURL
+          },
+          "fields": [
+            {
+              "name": "Creator",
+              "value": "tontool#2136, and creators of OhGodMusicBot",
+              "inline": true
+            },
+            {
+              "name": "Github Repository",
+              "value": "https://github.com/tontool/LitBot",
+              "inline": true
+            },
+            {
+              "name": "Invite link",
+              "value": "https://discordapp.com/api/oauth2/authorize?client_id=370613072181854218&permissions=36759552&scope=bot",
+              "inline": true
+            },
+            {
+              "name": "Uptime",
+              "value": days+" days, "+hrs+" Hrs, "+mnts+" Minutes, "+seconds+" Seconds",
+              "inline": true
+            },
+          ]
+        }
+      }
+    )
+
     },
     /*'reboot': (msg) => {
       if(hasRole(msg) === false) return;
